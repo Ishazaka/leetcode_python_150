ec811fd5739ffbd0a4414e963d7d9c4e4f85b119
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2024-11-02 15:35:41 -0400

    Medium

--- 11_Container_With_Most_Water.py
+++ 11_Container_With_Most_Water.py
@@ -15,3 +15,22 @@ class Solution:
         return res
 
 
+# Two Pointer
+#  Time & Space Complexity
+# Time complexity: O(n)
+# Space complexity: O(1)
+
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        l, r = 0, len(heights) - 1
+        res = 0
+
+        while l < r:
+            area = min(heights[l], heights[r]) * (r - l)
+            res = max(res, area)
+            if heights[l] <= heights[r]:
+                l += 1
+            else:
+                r -= 1
+        return res
+
