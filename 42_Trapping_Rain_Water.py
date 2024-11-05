2f36570349e593e9a05b7d71c3ce6ad7a8f3470a
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2024-11-05 10:37:15 -0500

    Hard

--- 42_Trapping_Rain_Water.py
+++ 42_Trapping_Rain_Water.py
@@ -1,4 +1,7 @@
 # Brute Force
+# Time complexity: O(n square)
+# Space complexity: O (1)
+
 class Solution:
     def trap(self, height: List[int]) -> int:
         if not height:
@@ -18,3 +21,27 @@ class Solution:
         return res
 
 
+# Prefix & Suffix Arrays
+# Time complexity: O(n)
+# Space complexity: O (n)
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        if n == 0:
+            return 0
+        
+        leftMax = [0] * n
+        rightMax = [0] * n
+        
+        leftMax[0] = height[0]
+        for i in range(1, n):
+            leftMax[i] = max(leftMax[i - 1], height[i])
+        
+        rightMax[n - 1] = height[n - 1]
+        for i in range(n - 2, -1, -1):
+            rightMax[i] = max(rightMax[i + 1], height[i])
+        
+        res = 0
+        for i in range(n):
+            res += min(leftMax[i], rightMax[i]) - height[i]
+        return res
