0671576e9123a51eaffbc3f2617a60402125bfd1
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2024-11-06 19:42:23 -0500

    Hard

--- 42_Trapping_Rain_Water.py
+++ 42_Trapping_Rain_Water.py
@@ -45,3 +45,27 @@ class Solution:
         for i in range(n):
             res += min(leftMax[i], rightMax[i]) - height[i]
         return res
+
+ # Stack
+# Time complexity: O(n)
+# Space complexity: O (n)
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+        stack = []
+        res = 0
+
+        for i in range(len(height)):
+            while stack and height[i] >= height[stack[-1]]:
+                mid = height[stack.pop()]
+                if stack:
+                    right = height[i]
+                    left = height[stack[-1]]
+                    h = min(right, left) - mid
+                    w = i - stack[-1] - 1
+                    res += h * w
+            stack.append(i)
+        return res
+
+
