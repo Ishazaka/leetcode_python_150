58a7606c2079f1ac3bb56e9db440c4200dca90b3
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2024-11-13 21:38:36 -0500

    Easy

---
+++ 20_Valid_Parentheses.py
@@ -0,0 +1,12 @@
+
+# Brute Force
+# Time complexity: O(n square)
+# Space complexity: O (n)
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        while '()' in s or '{}' in s or '[]' in s:
+            s = s.replace('()', '')
+            s = s.replace('{}', '')
+            s = s.replace('[]', '')
+        return s == ''
