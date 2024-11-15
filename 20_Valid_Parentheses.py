a1cb52614c8275e24a657de3dbbe54faa1ddbd58
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2024-11-14 23:33:50 -0500

    Easy

--- 20_Valid_Parentheses.py
+++ 20_Valid_Parentheses.py
@@ -10,3 +10,25 @@ class Solution:
             s = s.replace('{}', '')
             s = s.replace('[]', '')
         return s == ''
+
+
+# Stack
+# Time complexity: O(n)
+# Space complexity: O (n)
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closeToOpen = { ")" : "(", "]" : "[", "}" : "{" }
+
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        
+        return True if not stack else False
+
+
