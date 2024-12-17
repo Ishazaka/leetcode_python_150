c9e641950bbf4515a030d32a080516fc5eb7304b
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2024-12-16 21:11:41 -0500

    Easy

--- 290_Word_Pattern.py
+++ 290_Word_Pattern.py
@@ -18,3 +18,25 @@ class Solution:
             charToWord[c] = w
             wordToChar[w] = c
         return True
+
+
+
+// 2nd way
+
+
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        charToWord = {}
+        wordToChar = {}
+        words = s.split()
+
+        if len(pattern) != len(words):
+            return False
+
+        for i, (c, word) in enumerate(zip(pattern, words)):
+            if charToWord.get(c, 0) != wordToChar.get(word, 0):
+                return False
+            charToWord[c] = i + 1
+            wordToChar[word] = i + 1
+
+        return True
