6251397578f83adad0b60228f60b2ecbcd073cc2
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2024-12-22 23:47:22 -0500

    Easy

--- 58_Length_of_Last_Word.py
+++ 58_Length_of_Last_Word.py
@@ -1,4 +1,6 @@
 
+// 1st way
+
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         length = i = 0
@@ -12,4 +14,17 @@ class Solution:
             else:
                 length += 1
                 i += 1
+        return length
+
+
+// 2nd way
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        i, length = len(s) - 1, 0
+        while s[i] == ' ':
+            i -= 1
+        while i >= 0 and s[i] != ' ':
+            i -= 1
+            length += 1
         return length
