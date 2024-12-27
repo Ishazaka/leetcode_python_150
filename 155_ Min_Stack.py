f58808f1924ec9e1fa9239883505aa08663c7516
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2024-12-26 22:05:42 -0500

    Medium

--- 155_ Min_Stack.py
+++ 155_ Min_Stack.py
@@ -1,4 +1,5 @@
-
+# 1st way
+# brute force
 class MinStack:
 
     def __init__(self):
@@ -25,3 +26,25 @@ class MinStack:
             self.stack.append(tmp.pop())
         
         return mini
+
+# 2nd way
+#two stack
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
